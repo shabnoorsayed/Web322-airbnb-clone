568e90950bb6ddd0716a6ecbed41b78862a6272d
shabnoorsayed <shabnoor.sayed.ali@gmail.com>
2020-10-11 22:10:47 -0400

    Final commit

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 const path = require('path');
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/home.html'));
+    res.sendFile(path.join(__dirname + '/home.html'));
 });
 app.get("/room", (req, res) => {
-    res.sendFile(path.join(__dirname+'/room.html'));
+    res.sendFile(path.join(__dirname + '/room.html'));
 });
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname+'/register.html'));
+    res.sendFile(path.join(__dirname + '/register.html'));
 });
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname+'/login.html'));
+    res.sendFile(path.join(__dirname + '/login.html'));
 });
 
 // setup http server to listen on HTTP_PORT
